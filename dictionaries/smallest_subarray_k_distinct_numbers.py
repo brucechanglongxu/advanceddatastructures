b11bb67c7b4b749592615bcfa23f50d987935511
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 10:57:04 -0700

    Create smallest_subarray_k_distinct_numbers.py

---
+++ dictionaries/smallest_subarray_k_distinct_numbers.py
@@ -0,0 +1,5 @@
+# Given an array consisting of n integers and integer k, we need to find the minimum range in
+# the array [l, r] such that there are exactly k different numbers. If such subarray
+# doesn't exist, then we should print "invalid k"
+
+
