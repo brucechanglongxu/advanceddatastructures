e8e32c208c307c98ecafd5dbf88548e886c4b544
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 11:31:29 -0700

    Update max_subarray_sumk.py

--- dictionaries/max_subarray_sumk.py
+++ dictionaries/max_subarray_sumk.py
@@ -4,11 +4,12 @@
 
 class Solution:
 	"""
-	We use a prefix sum and hashmap approach
+	We use a prefix sum and hashmap approach O(n)
 	"""
 	def subarrayDivByK(self, A:List[int], K: int) -> int:
 		res = 0
 		curSum = 0
+		# the key represents the sum, and the value represents no. of times it has occured so far
 		prefixSums = { 0 : 1 }
 
 		for n in nums:
@@ -16,5 +17,5 @@ class Solution:
 			diff = curSum - k
 			res += prefixSums.get(diff, 0)		
 			prefixSums[diff] = 1 + prefixSums.get(curSum, 0)
-
+			
 		return res	
