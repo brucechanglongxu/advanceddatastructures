0ebc8920e7c83fca5a24496893a7ae343ec1e5d2
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 10:28:10 -0700

    Update subarray_nopairsum_divisiblebyk.py

--- dictionaries/subarray_nopairsum_divisiblebyk.py
+++ dictionaries/subarray_nopairsum_divisiblebyk.py
@@ -1,1 +1,3 @@
-# Given an array of N non-negative integers, task is to find the maximum size of a subarray such that the pairwise sum of the elements of this subarray is not divisible by a given integer, K. Also, print this subarray as well. If there are two or more subarrays that follow the above stated condition, then print the first one from the left. 
+# Given an array of N non-negative integers, task is to find the maximum size of a subarray such that the pairwise sum of the elements of this subarray
+# is not divisible by a given integer, K. Also, print this subarray as well. If there are two or more subarrays that follow the above stated condition, 
+# then print the first one from the left. 
